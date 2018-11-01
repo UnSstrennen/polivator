8e699a42edc51b6eab1575c82aa4cc42bcb80c29
Un Sstrennen <n-i-k-i-t@yandex.ru>
2018-11-01 20:42:30 +0300

    test code

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+#define min_hum_reg A0
+#define hum_sensor A1
+#define pump 2
 
+const byte HYSTERESIS = 5;
+void setup() {
+  pinMode(hum_sensor, INPUT);
+  pinMode(min_hum_reg, INPUT);
+  pinMode(pump, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  byte min_hum = map(analogRead(min_hum_reg), 0, 1023, 0, 100);
+  byte hum_now = map(analogRead(hum_sensor), 0, 1023, 0, 100);
+  // ниже цикл работы насоса
+  // в него мы не попадем, если условие при while не исполнится
+  while (hum_now <= min_hum - HYSTERESIS) {
+    digitalWrite(pump, HIGH);
+    byte hum_now = map(analogRead(hum_sensor, 0, 1023, 0, 100));
+    if (hum_now > min_hum + HYSTERESIS) {
+      digitalWrite(pump, LOW);
+      break;
+    }
+  }
 }
